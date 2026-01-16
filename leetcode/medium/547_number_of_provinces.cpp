9642ab083447d4394f92b699096c28c87e5f07ea
Yujen Hsieh <hyujens@gmail.com>
2026-01-17 01:28:55 +0800

    implement number of provinces

---
+++ leetcode/medium/547_number_of_provinces.cpp
@@ -0,0 +1,49 @@
+/*
+ * There are n cities. Some of them are connected, while some are not. If city a
+ * is connected directly with city b, and city b is connected directly with city
+ * then city a is connected indirectly with city c.
+ *
+ * A province is a group of directly or indirectly connected cities and no other
+ * cities outside of the group.
+ *
+ * You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the
+ * ith city and the jth city are directly connected, and isConnected[i][j] = 0
+ * otherwise.
+ *
+ * Return the total number of provinces.
+ */
+
+#include <queue>
+#include <unordered_set>
+#include <vector>
+
+int findCircleNum(std::vector<std::vector<int>> &isConnected) {
+  std::unordered_set<int> processing;
+  processing.reserve(isConnected.size());
+  for (int i = 0; i < isConnected.size(); i++)
+    processing.insert(i);
+
+  int provinces = 0;
+  while (!processing.empty()) {
+    // record a new province
+    provinces++;
+    std::queue<int> queue;
+    queue.push(*processing.begin());
+
+    while (!queue.empty()) {
+      int v = queue.front();
+      queue.pop();
+      processing.erase(v);
+
+      for (int i = 0; i < isConnected[v].size(); i++) {
+        // if the connected node can not be found in processing set,
+        // it means the node has been processed (visited).
+        if (isConnected[v][i] == 1 && processing.find(i) != processing.end()) {
+          queue.push(i);
+        }
+      }
+    }
+  }
+
+  return provinces;
+}
