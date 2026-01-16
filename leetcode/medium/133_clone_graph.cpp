b0a49467049324b1c32f0680921c673af9e135fa
Yujen Hsieh <hyujens@gmail.com>
2026-01-16 21:35:05 +0800

    add memory free logic for clone graph implementation

--- leetcode/medium/133_clone_graph.cpp
+++ leetcode/medium/133_clone_graph.cpp
@@ -52,3 +52,53 @@ Node *cloneGraph(Node *node) {
 
   return cloneHelper(node, records);
 }
+
+void deleteGraph(Node *node) {
+  if (node == nullptr)
+    return;
+
+  std::unordered_set<Node *> visited;
+  // 用vector來追蹤是因為要收集所有要處理的節點
+  // 如果用stack或queue，就會讓紀錄變得複雜
+  std::vector<Node *> q;
+
+  q.push_back(node);
+  visited.insert(node);
+
+  size_t head = 0; // 這是用來追蹤接下來要處理的node
+  while (head < q.size()) {
+    Node *curr = q[head];
+    head += 1; // 下一個要處理的
+    for (Node *neighbor : curr->neighbors) {
+      if (visited.find(neighbor) == visited.end()) {
+        // 第一次出現的node需要被處理
+        visited.insert(neighbor);
+        q.push_back(neighbor);
+      }
+    }
+  }
+
+  // 確認所有不重複的node
+  // 可以安全移除
+  for (Node *n : q)
+    delete n;
+}
+
+void deleteGraphHelper(Node *node, std::unordered_set<Node *> &candidates) {
+  if (node == nullptr || candidates.find(node) != candidates.end())
+    return;
+
+  candidates.insert(node);
+  for (Node *item : node->neighbors) {
+    if (candidates.find(item) == candidates.end())
+      deleteGraphHelper(item, candidates);
+  }
+}
+
+void deleteGraphV2(Node *node) {
+  std::unordered_set<Node *> candidates;
+  deleteGraphHelper(node, candidates);
+
+  for (Node *n : candidates)
+    delete n;
+}
